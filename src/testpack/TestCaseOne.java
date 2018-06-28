01a9be46869a7881798322d0d58210498c35d6cf
hemantharaj1 <hemantharaj1@gmail.com>
2018-06-28 12:08:03 +0530

    added the reverse by anu

--- src/testpack/TestCaseOne.java
+++ src/testpack/TestCaseOne.java
@@ -9,6 +9,7 @@ public class TestCaseOne {
 		TestCaseOne objuser1 = new TestCaseOne();
 		objuser1.add(4, 5);
 		objuser1.add(4, 5, 6);
+		objuser1.reverseByAnu();
 
 	}
 
@@ -23,4 +24,15 @@ public class TestCaseOne {
 	void add(int a, int b) {
 		System.out.println(a + b);
 	}
+public void reverseByAnu() {
+	String s="anu";
+	String temp="";
+	for (int i = s.length()-1; i >=0; i--) {
+		char c = s.charAt(i);
+		temp=temp+c;
+		
+	}
+	System.out.println(temp);
+	
+}
 }
