7388a6a3e293ae3caba827af3c2c4d3b30ace4ed
vinuanbu19 <vinorika19@gmail.com>
2018-06-28 12:12:29 +0530

    String Reverse

--- src/testpack/TestCaseOne.java
+++ src/testpack/TestCaseOne.java
@@ -9,7 +9,8 @@ public class TestCaseOne {
 		TestCaseOne objuser1 = new TestCaseOne();
 		objuser1.add(4, 5);
 		objuser1.add(4, 5, 6);
-
+        TestCaseOne rev = new TestCaseOne();
+        rev.reversePriya();
 	}
 
 	void thisismytestcaseone() {
@@ -23,4 +24,15 @@ public class TestCaseOne {
 	void add(int a, int b) {
 		System.out.println(a + b);
 	}
-}
+   public void reversePriya() {
+	   String Name = "Priya";
+	   String Reverse = "";
+	   int length = Name.length();
+	   for(int i = length-1; i>=0; i-- ) {
+		   Reverse = Reverse + Name.charAt(i);
+	   }
+	   System.out.println(Reverse);
+	   }
+   }
+
+
