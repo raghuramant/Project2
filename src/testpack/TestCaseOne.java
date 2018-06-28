3edcd69cfe69b798a9e7e00fca9a3338dc0e1867
raghuramant <raghuramanth@gmail.com>
2018-06-28 10:09:32 +0530

    added logic for add method

--- src/testpack/TestCaseOne.java
+++ src/testpack/TestCaseOne.java
@@ -4,12 +4,16 @@ public class TestCaseOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		TestCaseOne objuser1 = new TestCaseOne();
+		objuser1.add(4, 5);
 
 	}
 
 	void thisismytestcaseone() {
 		System.out.println("test case one logic");
 	}
-	
 
+	void add(int a, int b) {
+		System.out.println(a + b);
+	}
 }
