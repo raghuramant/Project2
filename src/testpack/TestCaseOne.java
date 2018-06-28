02440248d8b08296f616569e929594ba87458261
hemantharaj1 <hemantharaj1@gmail.com>
2018-06-28 22:33:25 +0530

    Added reverse string program

--- src/testpack/TestCaseOne.java
+++ src/testpack/TestCaseOne.java
@@ -4,11 +4,12 @@ public class TestCaseOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//TestCaseOne objuser2 = new TestCaseOne();
-		
+		// TestCaseOne objuser2 = new TestCaseOne();
+
 		TestCaseOne objuser1 = new TestCaseOne();
 		objuser1.add(4, 5);
 		objuser1.add(4, 5, 6);
+		objuser1.reverseByHemanth();
 
 	}
 
@@ -22,5 +23,15 @@ public class TestCaseOne {
 
 	void add(int a, int b) {
 		System.out.println(a + b);
+	}
+
+	void reverseByHemanth() {
+		String s = "Hemanth";
+		String rev = "";
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			rev = rev + s.charAt(i);
+		}
+		System.out.println(rev);
 	}
 }
