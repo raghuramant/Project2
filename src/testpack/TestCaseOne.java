b627aaf4a6f8dfa47dd247552ae3413d395afed4
vishnupriyan92 <vishnupriyan256@gmail.com>
2018-06-28 22:59:10 +0530

    added reverse program

--- src/testpack/TestCaseOne.java
+++ src/testpack/TestCaseOne.java
@@ -23,4 +23,14 @@ public class TestCaseOne {
 	void add(int a, int b) {
 		System.out.println(a + b);
 	}
+	void reverseByVishnu() {
+		String s = "Vishnu";
+		String rev = "";
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			rev = rev + s.charAt(i);
+		}
+		System.out.println(rev);
+}
+
 }
