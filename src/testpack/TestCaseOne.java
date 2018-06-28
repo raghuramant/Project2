e0892b54fe6c75e2cb91b3777678022ce45d5ead
raghuramant <raghuramanth@gmail.com>
2018-06-28 10:02:52 +0530

    initial commit

---
+++ src/testpack/TestCaseOne.java
@@ -0,0 +1,15 @@
+package testpack;
+
+public class TestCaseOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	void thisismytestcaseone() {
+		System.out.println("test case one logic");
+	}
+	
+
+}
