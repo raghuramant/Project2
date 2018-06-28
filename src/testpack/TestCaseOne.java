9cbaaab30e965ef62083aac3d05548322ae561fb
hemantharaj1 <hemantharaj1@gmail.com>
2018-06-28 11:35:20 +0530

    reversed my name vino

--- src/testpack/TestCaseOne.java
+++ src/testpack/TestCaseOne.java
@@ -9,6 +9,8 @@ public class TestCaseOne {
 		TestCaseOne objuser1 = new TestCaseOne();
 		objuser1.add(4, 5);
 		objuser1.add(4, 5, 6);
+		objuser1.reverseByVino();
+		
 
 	}
 
@@ -23,4 +25,13 @@ public class TestCaseOne {
 	void add(int a, int b) {
 		System.out.println(a + b);
 	}
+	public void reverseByVino() {
+		String name="vino";
+		String res="";
+		for (int i = name.length()-1; i >=0; i--) {
+			res=res+name.charAt(i);
+		}
+		System.out.println(res);
+
+}
 }
