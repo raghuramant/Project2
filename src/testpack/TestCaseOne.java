8671ccc31403f5f12ca94755993b69f87d867bb9
raghuramant <raghuramanth@gmail.com>
2018-06-28 10:11:46 +0530

    added some logic for add by user2

--- src/testpack/TestCaseOne.java
+++ src/testpack/TestCaseOne.java
@@ -4,12 +4,15 @@ public class TestCaseOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		TestCaseOne objuser2 = new TestCaseOne();
+		objuser2.add(4, 5, 6);
 	}
 
 	void thisismytestcaseone() {
 		System.out.println("test case one logic");
 	}
-	
 
+	void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
 }
