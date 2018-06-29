df49757bfef8b069494ed629f5e84b4c7dc2ddf1
jana8779 <janacse94@gmail.com>
2018-06-29 09:14:43 +0530

    reverse string

--- src/testpack/TestCaseOne.java
+++ src/testpack/TestCaseOne.java
@@ -4,11 +4,12 @@ public class TestCaseOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//TestCaseOne objuser2 = new TestCaseOne();
-		
+		// TestCaseOne objuser2 = new TestCaseOne();
+
 		TestCaseOne objuser1 = new TestCaseOne();
 		objuser1.add(4, 5);
 		objuser1.add(4, 5, 6);
+		objuser1.reverseByJana();
 
 	}
 
@@ -22,5 +23,17 @@ public class TestCaseOne {
 
 	void add(int a, int b) {
 		System.out.println(a + b);
+	}
+
+	public void reverseByJana() {
+
+		String s = "jana";
+		String split = "";
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			char ch = s.charAt(i);
+			split = split + ch;
+		}
+		System.out.println(split);
 	}
 }
